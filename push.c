2693629da1a58ffc19ab92095aa45eb2bc78469b
bcart01v <bcart0v@gmail.com>
2023-12-13 21:19:37 -0600

    Lost changes?

--- push.c
+++ push.c
@@ -1,36 +1,47 @@
 #include "monty.h"
+
 /**
- * push - push node at the top of the list
- * @stack: stack to use
- * @line_number: line number of command
+ * push - Push a new node at the top of the stack
+ * @head: Pointer to the head of the stack
+ * @line_number: Line number of the command in the input file
  *
- * Return: void
+ * This function pushes a new node with the specified integer value onto the top
+ * of the stack pointed to by 'head'.
+ *
+ * @head: Pointer to the head of the stack.
+ * @line_number: Line number of the command in the input file.
  */
 void push(stack_t **head, unsigned int line_number)
 {
 	const char *token = NULL;
 	int n;
 	stack_t *new_node;
-	
+
 	if (line_number <= 0)
 		return;
+
 	token = strtok(NULL, " ");
 	if (!token)
 	{
-		fprintf(stderr, "L%u: usage: push integer", line_number);
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
 		return;
 	}
+
 	n = atoi(token);
+
 	new_node = malloc(sizeof(stack_t));
 	if (!new_node)
 	{
-		printf("Error: malloc failed");
+		fprintf(stderr, "Error: malloc failed\n");
 		return;
 	}
+
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = *head;
-	if(*head)
+
+	if (*head)
 		(*head)->prev = new_node;
+
 	*head = new_node;
 }
